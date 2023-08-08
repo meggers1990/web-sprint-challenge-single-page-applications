854e6800fa8a4df5dd0d9a1851689b4205b16b59
megan huckabay <huckabay90@gmail.com>
2023-08-08 12:54:57 -0700

    passing all test

--- src/Component/Validation.js
+++ src/Component/Validation.js
@@ -1,12 +1,11 @@
 import * as yup from 'yup';
 
-
 const schema = yup.object().shape({
-  name: yup.string().required(' name must be at least 2 characters'),
-  size: yup.string().required(' special field is required'),
+  name: yup.string().required('Name is required').min(2, 'name must be at least 2 characters'),
+  size: yup.string().required('Size is required'),
   toppings: yup.array().of(yup.string()),
   special: yup.string(),
 });
 
 // Exports
-export default schema
+export default schema;
